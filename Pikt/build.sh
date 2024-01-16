1f61662a32861750d3f561819ecb2802ce920b8b
Patten Whiting <patten.whiting@gmail.com>
2024-01-15 21:15:15 -0700

    build.sh test

--- Pikt/build.sh
+++ Pikt/build.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
-# set -o errexit
-
-
+# build.sh
+echo "Starting application..."
 python manage.py collectstatic --no-input
-# python manage.py migrate
+ls -la /opt/render/project/src/Pikt/staticfiles
+gunicorn Pikt.wsgi:application --bind 0.0.0.0:10000
+ls -la /opt/render/project/src/Pikt/staticfiles
