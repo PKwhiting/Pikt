3f0dedc3f508d596dbddd7e3892703ed5830be5a
Patten Whiting <patten.whiting@gmail.com>
2024-01-15 21:33:13 -0700

    build.sh change

--- Pikt/build.sh
+++ Pikt/build.sh
@@ -3,5 +3,4 @@ echo "Starting application..."
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 ls -la /opt/render/project/src/Pikt/staticfiles
-gunicorn Pikt.wsgi:application --bind 0.0.0.0:10000
 ls -la /opt/render/project/src/Pikt/staticfiles
