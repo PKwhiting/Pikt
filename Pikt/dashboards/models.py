57a2fc54042ec535dac72407533da77fed4dff67
Patten Whiting <patten.whiting@gmail.com>
2024-01-20 17:55:25 -0700

    adding the model

--- Pikt/dashboards/models.py
+++ Pikt/dashboards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 PART_GRADES = (
     ('A', 'A'),
@@ -31,15 +33,15 @@ class part(models.Model):
     vehicle_trim = models.CharField(max_length=50)
     vehicle_engine = models.CharField(max_length=50)
     vehicle_color = models.CharField(max_length=50)
-    part_type = models.CharField(max_length=50)
-    part_fitment_location = models.CharField(max_length=255)
-    part_grade = models.CharField(max_length=1, choices=PART_GRADES)
-    part_interchange = models.CharField(max_length=50)
-    part_notes = models.CharField(max_length=255)
-    part_status = models.CharField(max_length=50, choices=PART_STATUS, default='Pending')
+    type = models.CharField(max_length=50)
+    fitment_location = models.CharField(max_length=255)
+    grade = models.CharField(max_length=1, choices=PART_GRADES)
+    hollander_interchange = models.CharField(max_length=50)
+    notes = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=PART_STATUS, default='Pending')
     vehicle_fitment = models.CharField(max_length=500)
-    cost = models.CharField(max_length=500, null=True, blank=True)
-    price = models.CharField(max_length=500, null=True, blank=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(Decimal('0.00'))])
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True, validators=[MinValueValidator(Decimal('0.00'))])
     part_image_1 = models.ImageField(upload_to='images/', null=True, blank=True)
     part_image_2 = models.ImageField(upload_to='images/', null=True, blank=True)
     part_image_3 = models.ImageField(upload_to='images/', null=True, blank=True)
@@ -53,5 +55,5 @@ class part(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     ebay_link = models.CharField(max_length=255, null=True, blank=True)
     def __str__(self):
-        return f'{self.vehicle_year} {self.vehicle_make} {self.vehicle_model} {self.vehicle_trim} {self.vehicle_engine} {self.part_type}'
+        return f'{self.vehicle_year} {self.vehicle_make} {self.vehicle_model} {self.vehicle_trim} {self.vehicle_engine} {self.type}'
         
