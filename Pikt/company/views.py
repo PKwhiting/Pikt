7cbf574d1f30943ea0265ba601d220e88eb6aef6
Patten Whiting <patten.whiting@gmail.com>
2024-01-28 16:43:04 -0700

    location query fix

--- Pikt/company/views.py
+++ Pikt/company/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from Authentication.models import User
+from .models import Location
 
 # Create your views here.
 class rootView(View):
     def get(self, request):
         company = request.user.company
         users = User.objects.filter(company=company).exclude(id=request.user.id)
-        locations = company.location_set.all()
+        locations = Location.objects.filter(company=company)
         context = {
             'users': users,
             'company': company,
